ca3a565c6fa5b2ea18ad24d898df220bb9680dd0
nakata122 <redspot122@gmail.com>
2025-05-24 11:24:37 +0300

    IOS stuff

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,5 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import { useEffect, useState } from 'react';
+import { generateToken, messaging } from '../firebase';
 
 function Layout() {
   const [user, setUser] = useState(0);
@@ -12,6 +13,7 @@ function Layout() {
   }, []);
 
   const changeUser = () => {
+    generateToken();
     localStorage.setItem('user', (parseInt(localStorage.getItem('user')) + 1) % 2);
     setUser(parseInt(localStorage.getItem('user')))
   }
